875ac87d3d22187f9bbd69478707ce49a5e82f2d
interrobang3544 <83446284+interrobang3544@users.noreply.github.com>
2021-09-15 21:19:16 +0900

    Add files via upload

--- lotto.js
+++ lotto.js
@@ -1,82 +1,101 @@
+const form = document.querySelector(".form"),
+  input = form.querySelector("input"),
+  userInput = document.querySelector(".user-input");
+ 
+function askForNum() {
+  form.addEventListener("submit", handleSubmit)
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    paintInput(input.value);
+    generateBalls(input.value);
+}
+  
+function paintInput(text){
+  form.classList.add('hide');
+  userInput.innerText = `${text}개 구매하셨습니다.
+  *다시 하시려면 새로고침 해주세요.`
+}
+
+askForNum();
+
+
 const result = document.getElementById('result');
 var pickedNum = [];
 var bonusNum = [];
 var newDiv = [];
-var a = 5;
 
-for (let i=0; i<a; i++){
-  newDiv.push(document.createElement('div'));
-  newDiv[i].className = `result${i}`;
-  newDiv[i].id = `result${i}`;
-  result.appendChild(newDiv[i]);
-
-  var candidates = Array(45)
-  .fill()
-  .map(function (element, index) {
-    return index + 1;
-  });
+function generateBalls(input) {
+  for (let i=0; i<input; i++){
+    newDiv.push(document.createElement('div'));
+    newDiv[i].className = `result${i}`;
+    newDiv[i].id = `result${i}`;
+    result.appendChild(newDiv[i]);
   
-  var shuffle = [];
-  while (candidates.length > 0) {
-    var value = candidates.splice(Math.floor(Math.random() * candidates.length), 1)[0];
-    shuffle.push(value);
-  }
-  var bonus = shuffle[shuffle.length - 1];
-  var picked = shuffle
-    .slice(0, 6)
-    .sort(function (p, c) {
-      return p - c;
+    var candidates = Array(45)
+    .fill()
+    .map(function (element, index) {
+      return index + 1;
     });
-
-  bonusNum[i] = bonus;
-  pickedNum[i] = picked;
-
-  function painting(number, resultN) {
-    var ball = document.createElement('div');
-    var ballSticker  = document.createElement('div');
-    var ballNumber = document.createElement('div');
     
-    ball.className = 'ball';
-    ballSticker.className = 'sticker';
-    ballNumber.className = 'num';
-    ballNumber.textContent = number;
-
-    var ballColor;
-    if (number <= 10) {
-      ballColor = 'red';
-    } else if (number <= 20) {
-      ballColor = 'orange';
-    } else if (number <= 30) {
-      ballColor = 'yellow';
-    } else if (number <= 40) {
-      ballColor = 'blue';
-    } else {
-      ballColor = 'green';
+    var shuffle = [];
+    while (candidates.length > 0) {
+      var value = candidates.splice(Math.floor(Math.random() * candidates.length), 1)[0];
+      shuffle.push(value);
     }
-    ball.style.background = ballColor;
-    
-    resultN.appendChild(ball);
-    ball.appendChild(ballSticker);
-    ball.appendChild(ballNumber);
-  }
-
-  function texting(addText, resultN) {
-    var textBonus = document.createElement('div');
-    textBonus.textContent = addText;
-    textBonus.className = 'text';
-    resultN.appendChild(textBonus);
-  }
+    var bonus = shuffle[shuffle.length - 1];
+    var picked = shuffle
+      .slice(0, 6)
+      .sort(function (p, c) {
+        return p - c;
+      });
   
-  for (let j = 0; j < picked.length; j++) {
+    bonusNum[i] = bonus;
+    pickedNum[i] = picked;
+  
+    function painting(number, resultN) {
+      var ball = document.createElement('div');
+      var ballNumber = document.createElement('div');
+      
+      ball.className = 'ball';
+      ballNumber.className = 'num';
+      ballNumber.textContent = number;
+  
+      var ballColor;
+      if (number <= 10) {
+        ballColor = 'red';
+      } else if (number <= 20) {
+        ballColor = 'orange';
+      } else if (number <= 30) {
+        ballColor = 'yellow';
+      } else if (number <= 40) {
+        ballColor = 'blue';
+      } else {
+        ballColor = 'green';
+      }
+      ball.style.background = ballColor;
+      
+      resultN.appendChild(ball);
+      ball.appendChild(ballNumber);
+    }
+  
+    function texting(addText, resultN) {
+      var textBonus = document.createElement('div');
+      textBonus.textContent = addText;
+      textBonus.className = 'textBonus';
+      resultN.appendChild(textBonus);
+    }
+    
+    for (let j = 0; j < picked.length; j++) {
+      setTimeout(() => {
+        painting(pickedNum[i][j], newDiv[i]);
+      }, (j + 1) * 100);
+    }
+  
     setTimeout(() => {
-      painting(pickedNum[i][j], newDiv[i]);
-    }, (j + 1) * 100);
-  }
-
-  setTimeout(() => {
-    console.log(i);
-    console.log(newDiv[i]);
-    texting('Bonus!', newDiv[i]);
-    painting(bonusNum[i], newDiv[i]);
-  }, 900);
+      texting('Bonus!', newDiv[i]);
+      painting(bonusNum[i], newDiv[i]);
+    }, 900);
+  }  
 }
